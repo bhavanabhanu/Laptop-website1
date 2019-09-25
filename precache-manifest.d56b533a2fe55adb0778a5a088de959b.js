797cf253d381c69102d3d2ac321209ab6b0a7fe3
sree bhavana <sreebhavananesa845@gmail.com>
2019-09-25 15:49:35 +0530

    LAPTOP PROJECT first commit

---
+++ precache-manifest.d56b533a2fe55adb0778a5a088de959b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486e05d4cb778d9ec21928007d6ee2f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe53fe1a68f364399f00",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "16d48c8e24f5a17c939f",
+    "url": "/static/css/main.faedaa05.chunk.css"
+  },
+  {
+    "revision": "fe53fe1a68f364399f00",
+    "url": "/static/js/2.1b2de5ef.chunk.js"
+  },
+  {
+    "revision": "16d48c8e24f5a17c939f",
+    "url": "/static/js/main.b6c80a8d.chunk.js"
+  },
+  {
+    "revision": "88af7aaabb899ef5f94b",
+    "url": "/static/js/runtime~main.f4e40734.js"
+  },
+  {
+    "revision": "54c895a0112e4133fc00444c4b830476",
+    "url": "/static/media/pexels-photo-1229861.54c895a0.jpeg"
+  }
+]);
